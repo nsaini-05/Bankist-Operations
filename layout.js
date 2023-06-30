3bfc0906111246b8d1108689cd403ae0845984e7
Neeraj Saini <asaini7788@gmail.com>
2023-06-30 13:14:43 -0600

    Display Balance and Compute Results

--- layout.js
+++ layout.js
@@ -1,10 +1,6 @@
 "use strict";
 
-/////////////////////////////////////////////////
-/////////////////////////////////////////////////
-// BANKIST APP
-
-// Data
+/* Data */
 const account1 = {
   owner: "Jonas Schmedtmann",
   transactions: [200, 450, -400, 3000, -650, -130, 70, 1300],
@@ -33,14 +29,28 @@ const account4 = {
   pin: 4444,
 };
 
-const transactions = [200, 450, -400, 3000, -650, -130, 70, 1300];
-
 const accounts = [account1, account2, account3, account4];
 
+/* DOM ELEMENTS */
 const transactionsSection = document.querySelector(".transactions");
+const balanceAmount = document.querySelector(".balance-amount");
 
+/* GET USERNAMES */
+const computeUserNames = (accounts) => {
+  for (const account of accounts) {
+    account.userName = account.owner
+      .toLowerCase()
+      .split(" ")
+      .map((str) => str[0])
+      .join("");
+  }
+};
+computeUserNames(accounts);
+
+/* DISPLAY TRANSACTIONS */
+
+const transactions = [200, 450, -400, 3000, -650, -130, 70, 1300];
 const displayTransactions = (transactions) => {
-  //Clearing the exisiting
   transactionsSection.innerHTML = "";
   transactions.forEach((transaction, index) => {
     const transactionType = transaction < 0 ? "Withdraw" : "Deposit";
@@ -56,3 +66,28 @@ const displayTransactions = (transactions) => {
 };
 
 displayTransactions(transactions);
+
+/* GET DEPOSITS */
+const deposits = transactions.filter((transaction) => transaction > 0);
+
+/* GET DEPOSITS */
+const withDraws = transactions.filter((transaction) => transaction < 0);
+
+/* DISPLAY BALANCE */
+const displayBalance = (transactions) => {
+  const balance = transactions.reduce((acc, cur) => acc + cur, 0);
+  balanceAmount.innerHTML = balance + "&nbsp$";
+};
+displayBalance(transactions);
+
+/* CALCULATE MAXIMUM TRANSACTION AMOUNT */
+
+const computeMaximumTransaction = (transactions) => {
+  const maxValue = transactions.reduce(
+    (acc, curr) => (acc = curr < acc ? acc : curr),
+    0
+  );
+  console.log(maxValue);
+};
+
+computeMaximumTransaction(transactions);
