df535b3a32e7d3ac5cb6a5da04207f091ae0aa10
nsaini-05 <asaini7788@gmail.com>
2023-07-01 12:57:18 -0600

    Login and Initial Data

--- index.js
+++ index.js
@@ -1,3 +1,82 @@
-document.querySelector(".btn").addEventListener("click", (e) => {
-  console.log("ASdafadsf");
+//Select DOM Elements
+const userNameField = document.querySelector(".username");
+const passwordField = document.querySelector(".password");
+const errorMessage = document.querySelector(".error");
+
+const getloggedInUser = () => {
+  const loggedInUser = localStorage.getItem("loggedInUser");
+  if (loggedInUser) {
+    window.location.href = "/layout.html";
+  }
+};
+
+getloggedInUser();
+
+const account1 = {
+  owner: "Jonas Schmedtmann",
+  transactions: [200, 450, -400, 3000, -650, -130, 70, 1300],
+  interestRate: 1.2, // %
+  pin: 1111,
+};
+
+const account2 = {
+  owner: "Jessica Davis",
+  transactions: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
+  interestRate: 1.5,
+  pin: 2222,
+};
+
+const account3 = {
+  owner: "Steven Thomas Williams",
+  transactions: [200, -200, 340, -300, -20, 50, 400, -460],
+  interestRate: 0.7,
+  pin: 3333,
+};
+
+const account4 = {
+  owner: "Sarah Smith",
+  transactions: [430, 1000, 700, 50, 90],
+  interestRate: 1,
+  pin: 4444,
+};
+
+const accounts = [account1, account2, account3, account4];
+/* GET USERNAMES */
+const computeUserNames = (accounts) => {
+  for (const account of accounts) {
+    account.userName = account.owner
+      .toLowerCase()
+      .split(" ")
+      .map((str) => str[0])
+      .join("");
+  }
+};
+computeUserNames(accounts);
+/* DISPLAY ERROR */
+
+const displayError = (message) => {
+  errorMessage.textContent = message;
+  setTimeout(() => (errorMessage.textContent = ""), 1000);
+};
+
+/* LOGIN FUNCTION */
+
+const userLogin = (userName, password) => {
+  const userFound = accounts.find((account) => account.userName === userName);
+  if (userFound) {
+    if (password !== userFound.pin) {
+      displayError("Invalid Username or Password");
+    } else {
+      userFound["pin"] = "****";
+      localStorage.setItem("loggedInUser", JSON.stringify(userFound));
+      window.location.href = "/layout.html";
+    }
+  } else {
+    displayError("Invalid Username or Password");
+  }
+};
+
+document.querySelector(".login-form").addEventListener("submit", (e) => {
+  e.preventDefault();
+  userLogin(userNameField.value, Number(passwordField.value));
 });
--- layout.js
+++ layout.js
@@ -1,4 +1,32 @@
 "use strict";
+/* DOM ELEMENTS */
+const transactionsSection = document.querySelector(".transactions");
+const balanceAmount = document.querySelector(".balance-amount");
+const depositTotalCard = document.querySelector(".deposit-total-card");
+const withdrawTotalCard = document.querySelector(".withdraw-total-card");
+const interestTotalCard = document.querySelector(".interest-total-card");
+const currentUserLabel = document.querySelector(".username-label");
+const logOutButton = document.querySelector(".logout-btn");
+let currentUser;
+
+const getloggedInUser = () => {
+  const loggedInUser = localStorage.getItem("loggedInUser");
+  if (loggedInUser) {
+    currentUser = JSON.parse(loggedInUser);
+  } else {
+    window.location.href = "/";
+  }
+};
+
+getloggedInUser();
+
+/* LOGOUT USER FUNCTION */
+const logOutUser = () => {
+  localStorage.removeItem("loggedInUser");
+  window.location.href = "/";
+};
+
+logOutButton.addEventListener("click", logOutUser);
 
 /* Data */
 const account1 = {
@@ -31,13 +59,6 @@ const account4 = {
 
 const accounts = [account1, account2, account3, account4];
 
-/* DOM ELEMENTS */
-const transactionsSection = document.querySelector(".transactions");
-const balanceAmount = document.querySelector(".balance-amount");
-const depositTotalCard = document.querySelector(".deposit-total-card");
-const withdrawTotalCard = document.querySelector(".withdraw-total-card");
-const interestTotalCard = document.querySelector(".interest-total-card");
-
 /* GET USERNAMES */
 const computeUserNames = (accounts) => {
   for (const account of accounts) {
@@ -51,7 +72,6 @@ const computeUserNames = (accounts) => {
 computeUserNames(accounts);
 
 /* DISPLAY TRANSACTIONS */
-const transactions = [200, 450, -400, 3000, -650, -130, 70, 1300];
 const displayTransactions = (transactions) => {
   transactionsSection.innerHTML = "";
   transactions.forEach((transaction, index) => {
@@ -61,60 +81,69 @@ const displayTransactions = (transactions) => {
         <p class="tag ${transactionType}-tag">${
       index + 1
     } ${transactionType}</p>
-        <p class="transaction-amount">${transaction}&nbsp;$</p>
+        <p class="transaction-amount">${transaction.toFixed(2)}&nbsp;$</p>
       </div>`;
     transactionsSection.insertAdjacentHTML("afterbegin", htmlElement);
   });
 };
-displayTransactions(transactions);
 
 /* DISPLAY SUMMARY */
 const displaySummary = (transactions) => {
   const depositTotal = transactions
     .filter((transaction) => transaction > 0)
-    .reduce((acc, curr, index) => acc + curr, 0);
-  depositTotalCard.textContent = `${depositTotal} $`;
+    .reduce((bal, curr, index) => bal + curr, 0);
+  depositTotalCard.textContent = `${depositTotal.toFixed(2)} $`;
 
   const withdrawTotal = transactions
     .filter((transaction) => transaction < 0)
     .reduce((acc, curr, index) => acc + curr, 0);
-  withdrawTotalCard.textContent = `${Math.abs(withdrawTotal)} $`;
+  withdrawTotalCard.textContent = `${Math.abs(withdrawTotal).toFixed(2)} $`;
 
   const interestTotal = transactions
     .filter((transaction) => transaction > 0)
-    .map((transaction, i, array) => (transaction * 1.2) / 100)
+    .map(
+      (transaction, i, array) => (transaction * currentUser.interestRate) / 100
+    )
     .filter((interest) => interest > 1)
     .reduce((acc, int, index, array) => acc + int, 0);
-  interestTotalCard.textContent = `${interestTotal} $`;
+  interestTotalCard.textContent = `${interestTotal.toFixed(2)} $`;
 };
-displaySummary(transactions);
-
-/* GET DEPOSITS */
-const deposits = transactions.filter((transaction) => transaction > 0);
-
-/* GET DEPOSITS */
-const withDraws = transactions.filter((transaction) => transaction < 0);
 
 /* DISPLAY BALANCE */
 const displayBalance = (transactions) => {
   const balance = transactions.reduce((acc, cur) => acc + cur, 0);
-  balanceAmount.innerHTML = balance + "&nbsp$";
+  balanceAmount.innerHTML = balance.toFixed(2) + "&nbsp$";
 };
-displayBalance(transactions);
 
-/* CALCULATE MAXIMUM TRANSACTION AMOUNT */
-const computeMaximumTransaction = (transactions) => {
-  const maxValue = transactions.reduce(
-    (acc, curr) => (acc = curr < acc ? acc : curr),
-    0
-  );
+const loadInitialData = () => {
+  const { owner: userName, transactions } = currentUser;
+  displayTransactions(transactions);
+  displaySummary(transactions);
+  displayBalance(transactions);
+  currentUserLabel.textContent = userName.split(" ")[0];
 };
-computeMaximumTransaction(transactions);
 
-/* CALCULATE AVERAGE TRANSACTION AMOUNT */
-const getAvgWithDraw = (withDraws) => {
-  const avgWithDraw = withDraws.reduce((acc, cur, i, array) => {
-    return acc + cur / array.length;
-  }, 0);
-  return Math.abs(avgWithDraw).toFixed(2);
-};
+loadInitialData();
+
+// /* GET DEPOSITS */
+// const deposits = transactions.filter((transaction) => transaction > 0);
+
+// /* GET DEPOSITS */
+// const withDraws = transactions.filter((transaction) => transaction < 0);
+
+// /* CALCULATE MAXIMUM TRANSACTION AMOUNT */
+// const computeMaximumTransaction = (transactions) => {
+//   const maxValue = transactions.reduce(
+//     (acc, curr) => (acc = curr < acc ? acc : curr),
+//     0
+//   );
+// };
+// computeMaximumTransaction(transactions);
+
+// /* CALCULATE AVERAGE TRANSACTION AMOUNT */
+// const getAvgWithDraw = (withDraws) => {
+//   const avgWithDraw = withDraws.reduce((acc, cur, i, array) => {
+//     return acc + cur / array.length;
+//   }, 0);
+//   return Math.abs(avgWithDraw).toFixed(2);
+// };
