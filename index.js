29ce45a53fd786449f7cb6008acc2d2f0d710d38
nsaini-05 <asaini7788@gmail.com>
2023-07-14 00:18:17 -0600

    Update Screenshot

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const getloggedInUser = () => {
 getloggedInUser();
 
 const account1 = {
-  owner: "Jonas Schmedtmann",
+  owner: "Benjamin Thompson",
   transactions: [200, 455.23, -306.5, 25000, -642.21, -133.9, 79.97, 1300],
   interestRate: 1.2, // %
   pin: 1111,
@@ -32,7 +32,7 @@ const account1 = {
 };
 
 const account2 = {
-  owner: "Jessica Davis",
+  owner: "Emma Johnson",
   transactions: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
   interestRate: 1.5,
   pin: 2222,
